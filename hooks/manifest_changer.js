ccd6c3de87fed9c6592d20b8870ad9b85a9e369f
Paulo Solipa <psolipa@product-league.com>
2023-06-30 00:09:25 +0100

    test

--- hooks/manifest_changer.js
+++ hooks/manifest_changer.js
@@ -2,31 +2,33 @@ const fs = require('fs');
 const _ = require('lodash');
 const xml2js = require('xml2js');
 
+let usesSDK = [
+  'com.outsystems.plugins.keystore'
+];
 
 module.exports = function (context) {
   const parseString = xml2js.parseString;
   const builder = new xml2js.Builder();
-  const manifestPath = context.opts.projectRoot + '/platforms/android/app/src/main/AndroidManifest.xml';
+  const manifestPath = context.opts.projectRoot + '/platforms/android/CordovaLib/AndroidManifest.xml';
   const androidManifest = fs.readFileSync(manifestPath).toString();
 
   let manifestRoot;
 
-  if (androidManifest) {
+  if (androidManifest && usesSDK.length > 0) {
     parseString(androidManifest, (err, manifest) => {
       if (err) return console.error(err);
-
-      if(!manifest.manifest['$']['xmlns:tools']){
-        manifest.manifest['$']['xmlns:tools'] = 'http://schemas.android.com/tools';
-      }
 
       manifestRoot = manifest['manifest'];
       console.log(manifestRoot);
       if (!manifestRoot['uses-sdk']) {
         manifestRoot['uses-sdk'] = [];
       }
-      manifestRoot['uses-sdk'].push({'$': {'tools:overrideLibrary': 'com.outsystems.plugins.keystore'}});
+        usesSDK.forEach(use => manifestRoot['uses-sdk'].push({'$': {'tools:overrideLibrary':use}}));
 
         fs.writeFileSync(manifestPath, builder.buildObject(manifest));
+
+        console.log(`Added ${usesSDK.length} uses SDK:`);
+        usesSDK.forEach(use => console.log(` - ${use}`));
       }
     )}
   }
